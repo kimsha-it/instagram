8f4fc01604ded5b824a6a5d0e8657905b109caab
kimsha-it <kimsha0it@gmail.com>
2025-12-02 01:28:24 +0900

    layout

---
+++ src/main/java/com/example/instagram/controller/AuthController.java
@@ -0,0 +1,14 @@
+package com.example.instagram.controller;
+
+import com.example.instagram.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserService userService;
+
+}
---
+++ src/main/java/com/example/instagram/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package com.example.instagram.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class UserRepository extends JpaRepository<User, Long> {
+}
